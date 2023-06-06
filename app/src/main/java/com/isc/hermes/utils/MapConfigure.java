8216d117cf8e4c2315054fc37e69384025a920a9
Misax148 <axel.ayala.9412013@usalesiana.edu.bo>
2023-06-06 15:46:04 -0400

    docs: Javadocs for MapConfigure, MapDisplay and MainActivity added.

--- app/src/main/java/com/isc/hermes/utils/MapConfigure.java
+++ app/src/main/java/com/isc/hermes/utils/MapConfigure.java
@@ -5,7 +5,16 @@ import androidx.annotation.NonNull;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.Style;
 
+/**
+ * Class for configuring a MapboxMap object.
+ */
 public class MapConfigure {
+
+    /**
+     * Configures a MapboxMap object with the MAPBOX_STREETS style.
+     *
+     * @param mapboxMap the MapboxMap object to be configured
+     */
     public void configure(@NonNull MapboxMap mapboxMap) {
         mapboxMap.setStyle(Style.MAPBOX_STREETS);
     }
--- app/src/main/java/com/isc/hermes/view/MapDisplay.java
+++ app/src/main/java/com/isc/hermes/view/MapDisplay.java
@@ -5,44 +5,81 @@ import android.os.Bundle;
 import com.isc.hermes.utils.MapConfigure;
 import com.mapbox.mapboxsdk.maps.MapView;
 
+/**
+ * Class for displaying a map using a MapView object and a MapConfigure object.
+ */
 public class MapDisplay {
     private final MapView mapView;
     private final MapConfigure mapConfigure;
 
+    /**
+     * Constructor to create a MapDisplay object.
+     *
+     * @param mapView the MapView object to display the map
+     * @param mapConfigure the MapConfigure object to configure the map
+     */
     public MapDisplay(MapView mapView, MapConfigure mapConfigure) {
         this.mapView = mapView;
         this.mapConfigure = mapConfigure;
     }
 
+    /**
+     * Method for creating the map and configuring it using the MapConfigure object.
+     *
+     * @param savedInstanceState the saved state of the instance
+     */
     public void onCreate(Bundle savedInstanceState) {
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(mapConfigure::configure);
     }
 
+    /**
+     * Method for starting the MapView object instance.
+     */
     public void onStart() {
         mapView.onStart();
     }
 
+    /**
+     * Method for resuming the MapView object instance.
+     */
     public void onResume() {
         mapView.onResume();
     }
 
+    /**
+     * Method for pausing the MapView object instance.
+     */
     public void onPause() {
         mapView.onPause();
     }
 
+    /**
+     * Method for stopping the MapView object instance.
+     */
     public void onStop() {
         mapView.onStop();
     }
 
+    /**
+     * Method for handling low memory situations.
+     */
     public void onLowMemory() {
         mapView.onLowMemory();
     }
 
+    /**
+     * Method for destroying the MapView object instance.
+     */
     public void onDestroy() {
         mapView.onDestroy();
     }
 
+    /**
+     * Method for saving the state of the MapView object instance.
+     *
+     * @param outState the state of the instance to save
+     */
     public void onSaveInstanceState(Bundle outState) {
         mapView.onSaveInstanceState(outState);
     }
