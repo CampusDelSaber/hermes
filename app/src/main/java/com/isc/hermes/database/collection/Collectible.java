22c238e0995d41a6edd2f24e473db0aa61c47c63
luiggy_mamani <luiggy.mamani.12777190@usalesiana.edu.bo>
2023-06-06 21:23:42 -0400

    docs: added documentation for the Collectible interface

--- app/src/main/java/com/isc/hermes/database/collection/Collectible.java
+++ app/src/main/java/com/isc/hermes/database/collection/Collectible.java
@@ -4,8 +4,16 @@ import com.mongodb.client.MongoCollection;
 
 import org.bson.Document;
 
+/**
+ * Interface to implement the classes that represent a collection of the hermes database.
+ */
 public interface Collectible {
 
+    /**
+     * Method to obtain the connection with the collection to perform operations with it.
+     *
+     * @return collection connection.
+     */
     public MongoCollection<Document> getCollection();
 
 }
