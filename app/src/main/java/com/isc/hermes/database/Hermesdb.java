1fdb81cc2d7c9f86ee121e270b1353298df6085b
luiggy_mamani <luiggy.mamani.12777190@usalesiana.edu.bo>
2023-06-06 21:12:44 -0400

    docs: added documentation for the Hermesdb class

--- app/src/main/java/com/isc/hermes/database/Hermesdb.java
+++ app/src/main/java/com/isc/hermes/database/Hermesdb.java
@@ -2,12 +2,20 @@ package com.isc.hermes.database;
 
 import com.mongodb.client.MongoDatabase;
 
+/**
+ * This class is responsible for generating the connection to the hermes database.
+ */
 public class Hermesdb {
 
     private Connection connection;
     private MongoDatabase database;
     private static Hermesdb hermesdb;
 
+    /**
+     * It is a singleton method to have a single instance of the hermes database connection.
+     *
+     * @return only connection created.
+     */
     public static Hermesdb getInstance() {
         if (hermesdb == null)
             hermesdb = new Hermesdb();
@@ -15,10 +23,18 @@ public class Hermesdb {
         return hermesdb;
     }
 
+    /**
+     * This is a constructor method to initialize their variables.
+     */
     private Hermesdb() {
         this.connection = Connection.getConnection();
     }
 
+    /**
+     * This is a method returns the hermes database obtained.
+     *
+     * @return only hermes database connection.
+     */
     public MongoDatabase getHermesDatabase() {
         if (database == null)
             database = connection.getDatabase().getDatabase("hermesmapdb");
