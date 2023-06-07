28e682293d70527bfb45f08d5dd0fcfcd6fdc669
fabianromeroclaros <fabian.romero.9385580@usalesiana.edu.bo>
2023-06-07 00:31:40 -0400

    feat:get current location by pressing button

--- app/src/main/java/com/isc/hermes/MainActivity.java
+++ app/src/main/java/com/isc/hermes/MainActivity.java
@@ -2,20 +2,49 @@ package com.isc.hermes;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.isc.hermes.utils.MapConfigure;
 import com.isc.hermes.view.MapDisplay;
+import com.mapbox.android.core.location.LocationEngine;
+import com.mapbox.android.core.location.LocationEngineCallback;
+import com.mapbox.android.core.location.LocationEngineProvider;
+import com.mapbox.android.core.location.LocationEngineRequest;
+import com.mapbox.android.core.location.LocationEngineResult;
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.permissions.PermissionsManager;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions;
+import com.mapbox.mapboxsdk.location.LocationComponentOptions;
+import com.mapbox.mapboxsdk.location.modes.CameraMode;
+import com.mapbox.mapboxsdk.location.modes.RenderMode;
 import com.mapbox.mapboxsdk.maps.MapView;
+
+import java.lang.ref.WeakReference;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * Class for displaying a map using a MapView object and a MapConfigure object.
+ * Handles current user location functionality.
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements PermissionsListener {
     private MapView mapView;
     private MapDisplay mapDisplay;
+    private Button locationButton;
+
+    private PermissionsManager permissionsManager;
+    private LocationEngine locationEngine;
+    private LocationEngineRequest locationEngineRequest;
 
     /**
      * Method for creating the map and configuring it using the MapConfigure object.
@@ -29,7 +58,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initMapView();
         initMapDisplay();
+        initLocationButton();
         mapDisplay.onCreate(savedInstanceState);
+        locationEngine = LocationEngineProvider.getBestLocationEngine(this);
     }
 
     /**
@@ -51,6 +82,24 @@ public class MainActivity extends AppCompatActivity {
      */
     private void initMapDisplay() {
         mapDisplay = new MapDisplay(mapView, new MapConfigure());
+    }
+
+    /**
+     * Method for initializing the location button and setting its click listener.
+     */
+    @SuppressLint("WrongViewCast")
+    private void initLocationButton() {
+        locationButton = findViewById(R.id.locationButton);
+        locationButton.setOnClickListener(v -> checkLocationPermissionsAndEnableComponent());
+    }
+
+    private void checkLocationPermissionsAndEnableComponent() {
+        if (PermissionsManager.areLocationPermissionsGranted(MainActivity.this)) {
+            enableLocationComponent();
+        } else {
+            permissionsManager = new PermissionsManager(MainActivity.this);
+            permissionsManager.requestLocationPermissions(MainActivity.this);
+        }
     }
 
     /**
@@ -116,5 +165,95 @@ public class MainActivity extends AppCompatActivity {
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
         mapDisplay.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onExplanationNeeded(List<String> list) {
+
+    }
+
+    /**
+     * Method called when permissions are granted.
+     */
+    @Override
+    public void onPermissionResult(boolean granted) {
+        if (granted) {
+            enableLocationComponent();
+        } else {
+            Toast.makeText(this, "Location permission denied.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Method called when the location engine is successfully connected.
+     */
+    private void onLocationEngineConnected() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationEngineRequest = new LocationEngineRequest.Builder(1000)
+                    .setPriority(LocationEngineRequest.PRIORITY_HIGH_ACCURACY)
+                    .build();
+
+            locationEngine.requestLocationUpdates(locationEngineRequest, new LocationListeningCallback(this), getMainLooper());
+        } else {
+            // Handle location permission not granted
+            Toast.makeText(this, "Location permission denied.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Method for enabling the location component on the map.
+     */
+    @SuppressWarnings("MissingPermission")
+    private void enableLocationComponent() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            LocationComponentOptions locationComponentOptions = LocationComponentOptions.builder(this)
+                    .pulseEnabled(true)
+                    .build();
+
+            LocationComponentActivationOptions locationComponentActivationOptions = LocationComponentActivationOptions.builder(this, Objects.requireNonNull(mapDisplay.getMapboxMap().getStyle()))
+                    .locationComponentOptions(locationComponentOptions)
+                    .build();
+
+            mapDisplay.getMapboxMap().getLocationComponent().activateLocationComponent(locationComponentActivationOptions);
+            mapDisplay.getMapboxMap().getLocationComponent().setLocationComponentEnabled(true);
+            mapDisplay.getMapboxMap().getLocationComponent().setCameraMode(CameraMode.TRACKING);
+            mapDisplay.getMapboxMap().getLocationComponent().setRenderMode(RenderMode.COMPASS);
+
+            onLocationEngineConnected();
+        } else {
+            permissionsManager = new PermissionsManager(this);
+            permissionsManager.requestLocationPermissions(this);
+        }
+    }
+
+    /**
+     * Callback class for receiving location updates.
+     */
+    private static class LocationListeningCallback implements LocationEngineCallback<LocationEngineResult> {
+        private final WeakReference<MainActivity> activityWeakReference;
+
+        LocationListeningCallback(MainActivity activity) {
+            this.activityWeakReference = new WeakReference<>(activity);
+        }
+
+        @Override
+        public void onSuccess(LocationEngineResult result) {
+            MainActivity activity = activityWeakReference.get();
+            if (activity != null) {
+                Location location = result.getLastLocation();
+                if (location != null) {
+                    LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                    activity.mapDisplay.getMapboxMap().animateCamera(com.mapbox.mapboxsdk.camera.CameraUpdateFactory.newLatLngZoom(latLng, 16.0));
+                }
+            }
+        }
+
+        @Override
+        public void onFailure(@NonNull Exception exception) {
+            MainActivity activity = activityWeakReference.get();
+            if (activity != null) {
+                Toast.makeText(activity, "Failed to get current location.", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
