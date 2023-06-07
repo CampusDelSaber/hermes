1d2bdc6d59e0cec1674ac1eaf3ea3f83ebef4635
luiggy_mamani <luiggy.mamani.12777190@usalesiana.edu.bo>
2023-06-06 21:23:59 -0400

    docs: added documentation for the Incidents class

--- app/src/main/java/com/isc/hermes/database/collection/Incidents.java
+++ app/src/main/java/com/isc/hermes/database/collection/Incidents.java
@@ -5,6 +5,9 @@ import com.mongodb.client.MongoCollection;
 
 import org.bson.Document;
 
+/**
+ * This class that represents the collection of incidents from the hermes database.
+ */
 public class Incidents implements Collectible {
 
     private Hermesdb hermesdb;
@@ -12,7 +15,11 @@ public class Incidents implements Collectible {
     private MongoCollection<Document> incidents;
     private static Collectible collection;
 
-
+    /**
+     * It is a singleton method to have a single instance of the incident collection.
+     *
+     * @return incident collection.
+     */
     public static Collectible getInstance() {
         if (collection == null)
             collection = new Incidents();
@@ -20,19 +27,30 @@ public class Incidents implements Collectible {
         return collection;
     }
 
+    /**
+     * This is a constructor method to initialize their variables.
+     */
     private Incidents() {
         this.COLLECTION_NAME = "incidents";
         this.hermesdb = Hermesdb.getInstance();
     }
 
+    /**
+     * This method returns the connection to the collection of incidents created.
+     *
+     * @return incidents collection.
+     */
     public MongoCollection<Document> getCollection() {
         if (incidents == null)
-            iniCollection();
+            initCollection();
 
         return incidents;
     }
 
-    private void iniCollection() {
+    /**
+     * This method gets the connection to the incident connection using the hermes database.
+     */
+    private void initCollection() {
         incidents = hermesdb.getHermesDatabase().getCollection(COLLECTION_NAME);
     }
 }
