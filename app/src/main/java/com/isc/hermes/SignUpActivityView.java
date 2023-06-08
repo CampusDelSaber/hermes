519ce372778f9a8fd15980836569bc01caca9e8f
Daniel Espinoza <daniel.espinoza.8812451@usalesiana.edu.bo>
2023-06-07 22:28:08 -0400

    feat:SignInByThirdParty-Services Implemented the authentication functionality with third party service and now it is possible to register with google.

--- app/src/main/java/com/isc/hermes/SignUpActivityView.java
+++ app/src/main/java/com/isc/hermes/SignUpActivityView.java
@@ -2,13 +2,29 @@ package com.isc.hermes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+
+import com.isc.hermes.controller.authentication.AuthenticationFactory;
+import com.isc.hermes.controller.authentication.AuthenticationServices;
+import com.isc.hermes.controller.authentication.IAuthentication;
+
+import java.util.HashMap;
+
+import java.util.Map;
+import java.util.Objects;
 
 
 /**
- * Class for displaying a log in view.
+ * This class is in charge of controlling the user's authentication activity.
  */
 public class SignUpActivityView extends AppCompatActivity {
+
+    private Map<Integer, IAuthentication> authenticationServices;
+    private IAuthentication authenticator;
 
     /**
      * Method for creating the view and configuring it using the components xml.
@@ -19,5 +35,72 @@ public class SignUpActivityView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_view);
+        initiateAuthenticationServices();
     }
+    /**
+     * This method Initializes the authentication services.
+     *<p>
+     * This method creates and initializes the available authentication services.
+     * It uses the AuthenticationServices enumeration to retrieve the possible values
+     * and create the corresponding authentication objects.
+     * </p>
+     */
+    private void initiateAuthenticationServices() {
+        authenticationServices = new HashMap<>();
+        for (AuthenticationServices value : AuthenticationServices.values()) {
+            authenticationServices.put(value.getID(), AuthenticationFactory.createAuthentication(value));
+            Objects.requireNonNull(authenticationServices.get(value.getID())).configureAccess(this);
+        }
+    }
+
+    /**
+     * This method is Called when the activity is starting or resuming.
+     *<p>
+     *Checks the authentication status of each authentication service to confirm if there is an active session.
+     *</p>
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        authenticationServices.forEach((key, authentication) -> {
+            if (authentication.checkUserSignIn(this)) {
+                Toast.makeText(SignUpActivityView.this,"The user is already authenticated",Toast.LENGTH_SHORT).show();
+                //The functionality for sign should be here
+            }
+        });
+    }
+
+    /**
+     *This method allows the user to register the user
+     *
+     * @param view it contains the event info.
+     */
+    public void SignUp(View view) {
+        authenticator = authenticationServices.get(view.getId());
+        if (authenticator == null) return;
+        startActivityForResult(
+                authenticator.signIn()
+                , view.getId()
+        );
+    }
+
+    /**
+     * This method controls the activity produced in the activity
+     *
+     * @param requestCode The integer request code originally supplied to
+     *                    startActivityForResult(), allowing you to identify who this
+     *                    result came from.
+     * @param resultCode The integer result code returned by the child activity
+     * @param data An Intent, which can return result data to the caller
+     *
+     */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (authenticationServices.containsKey(requestCode)) {
+            authenticator.handleSignInResult(data);
+        }
+    }
+
+
 }
---
+++ app/src/main/java/com/isc/hermes/controller/authentication/IAuthentication.java
@@ -0,0 +1,42 @@
+package com.isc.hermes.controller.authentication;
+
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * This is an Interface for authentication functionality.
+ * <p>
+ * This interface defines the methods required for authentication functionality. Implementing classes should provide
+ * implementations for these methods to configure access.
+ * </p>
+ */
+public interface IAuthentication {
+    /**
+     * This method configures access for the authentication process.
+     *
+     * @param context The activity context.
+     */
+    void configureAccess(Context context);
+
+    /**
+     *This method initiates the sign-in process.
+     *
+     * @return An Intent representing the sign-in process.
+     */
+    Intent signIn();
+
+    /**
+     * This method checks the user's sign-in status.
+     *
+     * @param context The activity context.
+     * @return True if the user is signed in, false otherwise.
+     */
+    boolean checkUserSignIn(Context context);
+
+    /**
+     * Handles the sign-in result.
+     *
+     * @param data The Intent data containing the sign-in result.
+     */
+    void handleSignInResult(Intent data);
+}
