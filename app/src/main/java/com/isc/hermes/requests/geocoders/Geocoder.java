e5f845f6b59716480b8f7eba33576a088142872e
luiggy_mamani <luiggy.mamani.12777190@usalesiana.edu.bo>
2023-06-08 11:04:29 -0400

    docs: documentation for the geocoder class

--- app/src/main/java/com/isc/hermes/requests/geocoders/Geocoder.java
+++ app/src/main/java/com/isc/hermes/requests/geocoders/Geocoder.java
@@ -7,14 +7,26 @@ import com.mapbox.api.geocoding.v5.models.GeocodingResponse;
 import io.github.cdimascio.dotenv.Dotenv;
 import retrofit2.Response;
 
+/**
+ * This is a parent class for the geocoding and reverse geocoding child classes.
+ */
 public class Geocoder {
 
     protected Dotenv dotenv;
 
+    /**
+     * This is a constructor method to initialize dotenv getter.
+     */
     public Geocoder() {
         this.dotenv = Dotenv.load();
     }
 
+    /**
+     * This method get response using a mapbox geocoding created.
+     *
+     * @param mapboxGeocoding to get response about geocoding or reverse geocoding.
+     * @return geocoding information.
+     */
     protected CarmenFeature getInfoResponse(MapboxGeocoding mapboxGeocoding) {
         try {
             Response<GeocodingResponse> response = mapboxGeocoding.executeCall();
