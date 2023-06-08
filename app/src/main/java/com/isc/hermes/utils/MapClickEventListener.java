d6dbce0a6e98de9a26ef58d6bec8716062f60851
Gaston Gutierrez <gaston.gutierrez.9964359@usalesiana.edu.bo>
2023-06-08 19:09:10 -0400

    show point only on avenues or streets

--- app/src/main/java/com/isc/hermes/utils/MapClickEventListener.java
+++ app/src/main/java/com/isc/hermes/utils/MapClickEventListener.java
@@ -1,16 +1,21 @@
 package com.isc.hermes.utils;
 
 import android.content.Context;
+import android.graphics.PointF;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.isc.hermes.R;
+import com.mapbox.geojson.Feature;
 import com.mapbox.mapboxsdk.annotations.MarkerOptions;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
+
+import java.util.List;
 
 /**
  * Class to configure the event of do click on a map
@@ -46,16 +51,21 @@ public class MapClickEventListener implements MapboxMap.OnMapClickListener {
      */
     @Override
     public boolean onMapClick(@NonNull LatLng point) {
-
         latLngPoint = point;
-        MarkerOptions markerOptions = new MarkerOptions().position(latLngPoint);
-        mapboxMap.addMarker(markerOptions);
-        incident2Button.setVisibility(View.VISIBLE);
-        incident3Button.setVisibility(View.VISIBLE);
+
+        PointF screenPoint = mapboxMap.getProjection().toScreenLocation(latLngPoint);
+        List<Feature> features = mapboxMap.queryRenderedFeatures(screenPoint, null);
+
+        if (!features.isEmpty()) {
+            MarkerOptions markerOptions = new MarkerOptions().position(latLngPoint);
+            mapboxMap.addMarker(markerOptions);
+            incident2Button.setVisibility(View.VISIBLE);
+            incident3Button.setVisibility(View.VISIBLE);
+        } else {
+            Toast.makeText(context, "Haz clic en una calle o avenida", Toast.LENGTH_SHORT).show();
+        }
 
         return true;
-
-
     }
 
     public LatLng getLatLngPoint() {return latLngPoint;}
