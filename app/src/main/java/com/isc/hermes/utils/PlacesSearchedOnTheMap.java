34c26fdbaeac5c746065ff5d666a25904a614bc3
SebastianBarra <barra20sebastian02p19@gmail.com>
2023-06-08 21:26:55 -0400

    test creation of waypoints since there is no a searcher

--- app/src/main/java/com/isc/hermes/utils/PlacesSearchedOnTheMap.java
+++ app/src/main/java/com/isc/hermes/utils/PlacesSearchedOnTheMap.java
@@ -5,14 +5,17 @@ import com.mapbox.mapboxsdk.annotations.MarkerOptions;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PlacesSearchedOnTheMap {
     private final MapboxMap mapboxMap;
+    List<LatLng> pointList = new ArrayList<>();
 
     public PlacesSearchedOnTheMap(MapboxMap mapboxMap, Context context) {
         this.mapboxMap = mapboxMap;
-
+        pointTest();
+        displayPoint(pointList);
     }
 
     public void displayPoint(List<LatLng> pointList) {
@@ -21,5 +24,14 @@ public class PlacesSearchedOnTheMap {
             MarkerOptions markerOptions = new MarkerOptions().position(aux);
             mapboxMap.addMarker(markerOptions);
         }
+    }
+
+    private void pointTest(){
+        LatLng latLng = new LatLng(-17.365546, -66.173602);
+        LatLng latLng2 = new LatLng(-17.366484, -66.175582);
+        LatLng latLng3 = new LatLng(-17.371483, -66.176134);
+        pointList.add(latLng);
+        pointList.add(latLng2);
+        pointList.add(latLng3);
     }
 }
