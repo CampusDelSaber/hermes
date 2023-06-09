6b1a52c01d69288dcd298b942a4c8b535b2fdbd4
SebastianBarra <barra20sebastian02p19@gmail.com>
2023-06-08 21:37:48 -0400

    Adding Javadocs

--- app/src/main/java/com/isc/hermes/utils/PlacesSearchedManager.java
+++ app/src/main/java/com/isc/hermes/utils/PlacesSearchedManager.java
@@ -4,10 +4,19 @@ import android.content.Context;
 
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 
+/**
+ * This class manages the waypoints on the mapBox map.
+ */
 public class PlacesSearchedManager {
 
     PlacesSearchedOnTheMap placesSearchedOnTheMap;
 
+    /**
+     * This method is the constructor of the class in which we will instantiate the waypoints.
+     *
+     * @param mapboxMap the MapboxMap object to be configured.
+     * @param context Get global information about the environment of an application.
+     */
     public PlacesSearchedManager(MapboxMap mapboxMap, Context context){
         placesSearchedOnTheMap = new PlacesSearchedOnTheMap(mapboxMap, context);
     }
--- app/src/main/java/com/isc/hermes/utils/PlacesSearchedOnTheMap.java
+++ app/src/main/java/com/isc/hermes/utils/PlacesSearchedOnTheMap.java
@@ -8,16 +8,29 @@ import com.mapbox.mapboxsdk.maps.MapboxMap;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This Class creates waypoints which will be marked on the map.
+ */
 public class PlacesSearchedOnTheMap {
     private final MapboxMap mapboxMap;
     List<LatLng> pointList = new ArrayList<>();
 
+    /**
+     * This method is the class constructor.
+     *
+     * @param mapboxMap the MapboxMap object to be configured.
+     * @param context Get global information about the environment of an application.
+     */
     public PlacesSearchedOnTheMap(MapboxMap mapboxMap, Context context) {
         this.mapboxMap = mapboxMap;
         pointTest();
         displayPoint(pointList);
     }
 
+    /**
+     * This method creates and displays waypoints on the map.
+     * @param pointList list of places that will be marked on the map.
+     */
     public void displayPoint(List<LatLng> pointList) {
         for (int i = 0; i < pointList.size(); i++) {
             LatLng aux = pointList.get(i);
@@ -26,6 +39,11 @@ public class PlacesSearchedOnTheMap {
         }
     }
 
+    /**
+     * This method is for testing due to lack of functionality in the program.
+     *
+     * TODO: This method must be removed.
+     */
     private void pointTest(){
         LatLng latLng = new LatLng(-17.365546, -66.173602);
         LatLng latLng2 = new LatLng(-17.366484, -66.175582);
