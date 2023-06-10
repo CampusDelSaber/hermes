26160a3675a2360060f3b9db99a0b01ad756b142
jeferssoncl77 <jefersson.coronel.13622689@usalesiana.edu.bo>
2023-06-10 15:17:35 -0400

    fix: Fixed the unexpected closure of the 'about Us' button:
    Modifying the manifest file and making modifications fixed this button

--- app/src/main/java/com/isc/hermes/SignUpActivityView.java
+++ app/src/main/java/com/isc/hermes/SignUpActivityView.java
@@ -46,7 +46,7 @@ public class SignUpActivityView extends AppCompatActivity {
     private void creationActionAboutUs(){
         TextView btn = findViewById(R.id.bttn_about_us);
         btn.setOnClickListener(v -> startActivity(
-                new Intent(SignUpActivityView.this,AboutUs.class)));
+                new Intent(this,AboutUs.class)));
     }
 
     /**
