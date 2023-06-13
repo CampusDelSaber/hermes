dc779f61130711ef6d371e93b25d18837e49ae93
fabianromeroclaros <fabian.romero.9385580@usalesiana.edu.bo>
2023-06-13 11:50:42 -0400

    feat: api handler class implementation

--- app/src/main/java/com/isc/hermes/database/ApiHandler.java
+++ app/src/main/java/com/isc/hermes/database/ApiHandler.java
@@ -9,14 +9,29 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URL;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import timber.log.Timber;
 
 public class ApiHandler {
-    private String API_URL = "https://api-rest-hermes.onrender.com/incidents";
+    private final String API_URL = "https://api-rest-hermes.onrender.com/incidents";
+    private final String INCIDENTS_COLLECTION_NAME = "incidents";
+
+    private ExecutorService executorService;
+
+    public JSONArray getAllIncidents() throws ExecutionException, InterruptedException {
+        Future<String> future = executeService(API_URL + "/" + INCIDENTS_COLLECTION_NAME);
+        return getJSONArrayOnResult(future.get());
+    }
+
+    private Future<String> executeService(String apiUrl){
+        executorService = Executors.newSingleThreadExecutor();
+        return executorService.submit(() -> fetchDataFromApi(apiUrl));
+    }
 
     public String fetchDataFromApi(String apiUrl) {
         StringBuilder response = new StringBuilder();
@@ -29,12 +44,9 @@ public class ApiHandler {
             InputStream inputStream = connection.getInputStream();
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
             String line;
-            while ((line = bufferedReader.readLine()) != null) {
+            while ((line = bufferedReader.readLine()) != null)
                 response.append(line);
-            }
 
-            System.out.println(response);
-            System.out.println("response");
             bufferedReader.close();
             inputStream.close();
             connection.disconnect();
@@ -45,27 +57,32 @@ public class ApiHandler {
         return response.toString();
     }
 
-    public void handleApiResponse(String result) {
+    public JSONArray getJSONArrayOnResult(String result) {
         try {
-            JSONArray incidentsArray = new JSONArray(result);
-            Timber.tag("HOLA").d(result);
+            JSONArray jsonArray = new JSONArray(result);
 
-            // Process each incident object
-            for (int i = 0; i < incidentsArray.length(); i++) {
-                JSONObject incidentObj = incidentsArray.getJSONObject(i);
-                String id = incidentObj.getString("_id");
-                String type = incidentObj.getString("type");
-                String reason = incidentObj.getString("reason");
-                String dateCreated = incidentObj.getString("dateCreated");
-
-                // Process the incident data as needed
-                Timber.tag("Incident").d("ID: %s", id);
-                Timber.tag("Incident").d("Type: %s", type);
-                Timber.tag("Incident").d("Reason: %s", reason);
-                Timber.tag("Incident").d("Date Created: %s", dateCreated);
-            }
+            handleIncidentsDataExample(jsonArray);
+            return jsonArray;
         } catch (JSONException e) {
             e.printStackTrace();
+            return new JSONArray();
+        }
+    }
+
+    private void handleIncidentsDataExample(JSONArray incidentsArray) throws JSONException {
+        // Example of processing the data
+        for (int i = 0; i < incidentsArray.length(); i++) {
+            JSONObject incidentObj = incidentsArray.getJSONObject(i);
+            String id = incidentObj.getString("_id");
+            String type = incidentObj.getString("type");
+            String reason = incidentObj.getString("reason");
+            String dateCreated = incidentObj.getString("dateCreated");
+
+            // Process the incident data as needed example to log:
+            Timber.tag("Incident").d("ID: %s", id);
+            Timber.tag("Incident").d("Type: %s", type);
+            Timber.tag("Incident").d("Reason: %s", reason);
+            Timber.tag("Incident").d("Date Created: %s", dateCreated);
         }
     }
 }
