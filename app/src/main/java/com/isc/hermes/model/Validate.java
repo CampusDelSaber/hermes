9c865f4ccdf36e0059b06e0b4af10c0af4213ac9
Denis Jorge Gandarillas Delgado <denis.gandarillas.9355205@usalesiana.edu.bo>
2023-06-13 16:54:42 -0400

    FEATURE US-301

---
+++ app/src/main/java/com/isc/hermes/model/Validate.java
@@ -0,0 +1,16 @@
+package com.isc.hermes.model;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.isc.hermes.R;
+
+public class Validate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_validate);
+    }
+}
