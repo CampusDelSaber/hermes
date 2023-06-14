f286b4002bb340f65f6770147002ee15965321b3
luiggy_mamani <luiggy.mamani.12777190@usalesiana.edu.bo>
2023-06-13 21:27:08 -0400

    docs: documentation for the CoordinateParser class

--- app/src/main/java/com/isc/hermes/generators/CoordinateParser.java
+++ app/src/main/java/com/isc/hermes/generators/CoordinateParser.java
@@ -6,17 +6,38 @@ import org.locationtech.jts.geom.Geometry;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class has the responsibility to parse coordinate data.
+ */
 public class CoordinateParser {
 
+    /**
+     * This method parse double coordinate to coordinate type.
+     *
+     * @param coordinate is the double coordinate.
+     * @return coordinate type.
+     */
     public Coordinate doubleToCoordinate(Double[] coordinate) {
         return coordinate.length == 2 ?
                 new Coordinate(coordinate[0], coordinate[1]) : null;
     }
 
+    /**
+     * This method parse the coordinates of polygon to list of double coordinates.
+     *
+     * @param polygom is the polygon to ger coordinates data.
+     * @return double coordinates.
+     */
     public List<Double[]> parseToDoubles(Geometry polygom) {
         return parseToDoubles(List.of(polygom.getCoordinates()));
     }
 
+    /**
+     * This method parse coordinate coordinates list to double coordinate list.
+     *
+     * @param coordinates are the coordinates of coordinate types.
+     * @return double coordinates list.
+     */
     public List<Double[]> parseToDoubles(List<Coordinate> coordinates) {
         List<Double[]> doubleCoordinates = new ArrayList<>();
         coordinates.forEach(coordinate -> {
@@ -26,6 +47,12 @@ public class CoordinateParser {
         return doubleCoordinates;
     }
 
+    /**
+     * This method parse double coordinates to list of coordinate types.
+     *
+     * @param doubleCoordinates are the double coordinates.
+     * @return list of coordinates types.
+     */
     public List<Coordinate> parseToCoordinates(List<Double[]> doubleCoordinates) {
         List<Coordinate> coordinates = new ArrayList<>();
         doubleCoordinates.forEach(coordinate -> {
