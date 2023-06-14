31c1526e0028ae45ffb030ff1078990022fefd8e
jeferssoncl77 <jefersson.coronel.13622689@usalesiana.edu.bo>
2023-06-14 00:24:31 -0400

    feat: Implementation view and controller

---
+++ app/src/main/java/com/isc/hermes/AccountInformation.java
@@ -0,0 +1,13 @@
+package com.isc.hermes;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AccountInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_information);
+    }
+}
