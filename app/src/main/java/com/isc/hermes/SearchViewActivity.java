1fc93d2d243d89d1525c7c164b002c69cb7786eb
Misax148 <axel.ayala.9412013@usalesiana.edu.bo>
2023-06-14 10:50:25 -0400

    style: Searh View added

---
+++ app/src/main/java/com/isc/hermes/SearchViewActivity.java
@@ -0,0 +1,13 @@
+package com.isc.hermes;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SearchViewActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_view);
+    }
+}
