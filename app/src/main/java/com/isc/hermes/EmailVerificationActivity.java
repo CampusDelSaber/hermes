9296dc72ae847ea741bbdcb44cb46703b17e0484
Emanuel Galindo <emanuel.galindo.9362056@usalesiana.edu.bo>
2023-06-14 22:31:02 -0400

    Finishing feature

--- app/src/main/java/com/isc/hermes/EmailVerificationActivity.java
+++ app/src/main/java/com/isc/hermes/EmailVerificationActivity.java
@@ -8,57 +8,85 @@ import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class EmailVerificationActivity extends AppCompatActivity {
 
+
     private EditText[] codeEditTexts;
-    private EditText codeTextField1;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mail_verification);
+        initComponents();
+    }
 
-        // Obtener referencias a los EditText
-        codeTextField1 = findViewById(R.id.codeTextField1);
-        EditText codeTextField2 = findViewById(R.id.codeTextField2);
-        EditText codeTextField3 = findViewById(R.id.codeTextField3);
-        EditText codeTextField4 = findViewById(R.id.codeTextField4);
-        EditText codeTextField5 = findViewById(R.id.codeTextField5);
-        EditText codeTextField6 = findViewById(R.id.codeTextField6);
+    private void initComponents(){
+        codeEditTexts = new EditText[]{
+                findViewById(R.id.codeTextField1),
+                findViewById(R.id.codeTextField2),
+                findViewById(R.id.codeTextField3),
+                findViewById(R.id.codeTextField4),
+                findViewById(R.id.codeTextField5),
+                findViewById(R.id.codeTextField6)
+        };
 
-        // Crear un arreglo con los EditText restantes
-        codeEditTexts = new EditText[]{codeTextField2, codeTextField3, codeTextField4, codeTextField5, codeTextField6};
+        configureEditTexts();
+    }
 
-        // Configurar TextWatchers y OnFocusChangeListener para los EditText
+    private void configureEditTexts() {
         for (int i = 0; i < codeEditTexts.length; i++) {
             final EditText currentEditText = codeEditTexts[i];
             final EditText nextEditText = (i < codeEditTexts.length - 1) ? codeEditTexts[i + 1] : null;
 
-            currentEditText.addTextChangedListener(new TextWatcher() {
-                @Override
-                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                }
+            configureTextWatcher(currentEditText, nextEditText);
 
-                @Override
-                public void onTextChanged(CharSequence s, int start, int before, int count) {
-                }
-
-                @Override
-                public void afterTextChanged(Editable s) {
-                    if (s.length() == 1 && nextEditText != null) {
-                        nextEditText.requestFocus();
-                    }
-                }
-            });
-
-            currentEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-                @Override
-                public void onFocusChange(View v, boolean hasFocus) {
-                    if (hasFocus) {
-                        codeTextField1.requestFocus();
-                    }
-                }
-            });
         }
     }
+
+    private void configureTextWatcher(final EditText currentEditText, final EditText nextEditText) {
+
+        currentEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.length() == 2 && nextEditText != null){
+                    String lastCharacter = s.toString().substring(1);
+                    currentEditText.setText(s.subSequence(0,1));
+                    nextEditText.setFocusableInTouchMode(true);
+                    nextEditText.setText(lastCharacter);
+                    nextEditText.requestFocus();
+                    nextEditText.setSelection(nextEditText.getText().length());
+                    currentEditText.setFocusableInTouchMode(false);
+
+                }
+                else if (s.length() == 0 && currentEditText != codeEditTexts[0]){
+                    EditText previousEditText =  getPreviousEditText(currentEditText);
+                    previousEditText.setFocusableInTouchMode(true);
+                    previousEditText.requestFocus();
+                    previousEditText.setSelection(previousEditText.getText().length());
+                    currentEditText.setFocusableInTouchMode(false);
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+    }
+
+
+    private EditText getPreviousEditText(EditText currentEditText) {
+        for (int i = 1; i < codeEditTexts.length; i++) {
+            if (codeEditTexts[i] == currentEditText) {
+                return codeEditTexts[i - 1];
+            }
+        }
+        return null;
+    }
+
+
 }
