c6c862b1cbf109b6ef854f612b95f73b495bb2b8
victor0villca <victor.villca.9518420@usalesiana.edu.bo>
2023-06-16 09:12:38 -0400

    fix: merge confflicts

--- app/src/main/java/com/isc/hermes/view/MapDisplay.java
+++ app/src/main/java/com/isc/hermes/view/MapDisplay.java
@@ -1,35 +1,13 @@
 package com.isc.hermes.view;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.os.Bundle;
 
-<<<<<<< HEAD
 import com.isc.hermes.controller.IncidentsGetterController;
-=======
-import androidx.annotation.NonNull;
-
-import com.isc.hermes.model.Utils.PolylineManager;
-import com.isc.hermes.utils.MapClickEventsManager;
->>>>>>> 2821b69c7396e94cc7b4fa3720daf472b2fe84a5
 import com.isc.hermes.utils.MapConfigure;
-import com.mapbox.geojson.Feature;
-import com.mapbox.geojson.LineString;
-import com.mapbox.geojson.Point;
-import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
-import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
-import com.mapbox.mapboxsdk.plugins.annotation.LineManager;
-import com.mapbox.mapboxsdk.plugins.annotation.LineOptions;
-import com.mapbox.mapboxsdk.style.layers.LineLayer;
-import com.mapbox.mapboxsdk.style.layers.PropertyFactory;
-import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
-import com.mapbox.mapboxsdk.style.sources.Source;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 /**
@@ -56,12 +34,6 @@ public class MapDisplay {
         incidentsGetterController = IncidentsGetterController.getInstance();
     }
 
-<<<<<<< HEAD
-=======
-
-
-
->>>>>>> 2821b69c7396e94cc7b4fa3720daf472b2fe84a5
     /**
      * Method for creating the map and configuring it using the MapConfigure object.
      *
