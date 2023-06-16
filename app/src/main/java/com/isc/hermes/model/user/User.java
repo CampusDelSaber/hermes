eefa84a3a36e32ba9eeaa84c5928c3f4ab2b640b
Santiago C. Saavedra <gabriel.concha.13120697@usalesiana.edu.bo>
2023-06-15 21:08:51 -0400

    doc: user sign up

--- app/src/main/java/com/isc/hermes/model/user/User.java
+++ app/src/main/java/com/isc/hermes/model/user/User.java
@@ -1,5 +1,8 @@
 package com.isc.hermes.model.user;
 
+/**
+ * The User class holds a user data.
+ */
 public class User {
 
     private final Payload payload;
@@ -9,26 +12,54 @@ public class User {
         this.payload = payload;
     }
 
+    /**
+     * Retrieves the givenName and the familyName to merge it as the fullName.
+     *
+     * @return The fullName of the user.
+     */
     public String getFullName(){
         return String.format("%s %s", payload.getGivenName(), payload.getFamilyName());
     }
 
+    /**
+     * Retrieves the givenName.
+     *
+     * @return the givenName as userName
+     */
     public String getUserName(){
         return payload.getGivenName();
     }
 
+    /**
+     * Retrieves the emailAddress.
+     *
+     * @return the email address of the user
+     */
     public String getEmail(){
         return payload.getEmailAddress();
     }
 
+    /**
+     * Retrieves the URL path of the user as string.
+     *
+     * @return the URL path to the image of the user
+     */
     public String getPathImageUser(){
         return payload.getPhotoURL();
     }
 
+    /**
+     * Sets the user's role.
+     */
     public void setRole(UserRoles role){
         this.role = role;
     }
 
+    /**
+     * Retrieves the user's role.
+     *
+     * @return the role of the user
+     */
     public UserRoles getRole() {
         return role;
     }
