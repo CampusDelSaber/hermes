4c21d6bb343a4021dbc8fc6d7a3a98d75a8dc0e3
Misax148 <axel.ayala.9412013@usalesiana.edu.bo>
2023-06-19 23:56:47 -0400

    feat: Geocode Filters completed methods

--- app/src/main/java/com/isc/hermes/controller/GeocodeFiltersManager.java
+++ app/src/main/java/com/isc/hermes/controller/GeocodeFiltersManager.java
@@ -1,15 +1,23 @@
 package com.isc.hermes.controller;
 
+import static android.content.ContentValues.TAG;
+
 import android.app.Activity;
+
+import androidx.annotation.NonNull;
 import androidx.core.util.Consumer;
 import com.isc.hermes.R;
 import com.mapbox.api.geocoding.v5.GeocodingCriteria;
 import com.mapbox.api.geocoding.v5.MapboxGeocoding;
 import com.mapbox.api.geocoding.v5.models.CarmenFeature;
+import com.mapbox.api.geocoding.v5.models.GeocodingResponse;
 import com.mapbox.core.exceptions.ServicesException;
 import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import java.util.List;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import timber.log.Timber;
 
 public class GeocodeFiltersManager {
@@ -25,7 +33,7 @@ public class GeocodeFiltersManager {
                 .query(query)
                 .build();
 
-        // TODO: implemented manageGeoCodingResponse
+        manageGeoCodingResponse(mapboxGeocoding, callback);
     }
 
     public void makeGeocodeSearch(LatLng latLng, Consumer<List<CarmenFeature>> callback) {
@@ -37,11 +45,28 @@ public class GeocodeFiltersManager {
                     .mode(GeocodingCriteria.MODE_PLACES)
                     .build();
 
-            // TODO: implemented manageGeoCodingResponse
+            manageGeoCodingResponse(client, callback);
 
         } catch (ServicesException servicesException) {
             Timber.e("Error geocoding: %s", servicesException.toString());
             servicesException.printStackTrace();
         }
+    }
+
+    private void manageGeoCodingResponse(MapboxGeocoding mapboxGeocoding, Consumer<List<CarmenFeature>> callback) {
+        mapboxGeocoding.enqueueCall(new Callback<>() {
+            @Override
+            public void onResponse(@NonNull Call<GeocodingResponse> call, @NonNull Response<GeocodingResponse> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    List<CarmenFeature> results = response.body().features();
+                    callback.accept(results);
+                } else Timber.tag(TAG).d("onResponse: No result found");
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<GeocodingResponse> call, @NonNull Throwable throwable) {
+                throwable.printStackTrace();
+            }
+        });
     }
 }
