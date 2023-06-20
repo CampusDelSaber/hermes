7920ba18ceec03d29a780b33fc40842c1fdd303e
Victor Leon Villca Silva <victor.villca.9518420@usalesiana.edu.bo>
2023-06-20 17:19:05 -0400

    feat-test:create bidirectinal graph function and testing

--- app/src/main/java/com/isc/hermes/utils/GraphUtils.java
+++ app/src/main/java/com/isc/hermes/utils/GraphUtils.java
@@ -9,19 +9,44 @@ import java.util.Map;
 
 public class GraphUtils {
     private static GraphUtils instance;
-    public void traverseGraph(Graph graph) {
+    public String traverseGraph(Graph graph) {
         Map<String, Node> nodes = graph.getNodes();
-
+        StringBuilder stringBuilder = new StringBuilder();
         for (Node node : nodes.values()) {
             System.out.println("Street names in " + node.getId() + ":");
-
+            stringBuilder.append("Street names in " + node.getId() + ":");
             List<Edge> edges = node.getEdges();
             for (Edge edge : edges) {
                 System.out.println(edge.getSource().getId() + " -> " + edge.getDestination().getId());
+                stringBuilder.append(edge.getSource().getId() + " -> " + edge.getDestination().getId());
             }
-            System.out.println();
+            System.out.println("----*-----");
+            stringBuilder.append("----*-----");
         }
+        return stringBuilder.toString();
     }
+
+    public Graph createBidirectionalGraph(Graph originalGraph) {
+        Graph newGraph = new Graph();
+        Map<String, Node> originalNodes = originalGraph.getNodes();
+        for (Node originalNode : originalNodes.values()) {
+            Node newNode = new Node(originalNode.getId(), originalNode.getLatitude(), originalNode.getLongitude());
+            newGraph.addNode(newNode);
+        }
+        for (Node originalNode : originalNodes.values()) {
+            Node newNode = newGraph.getNode(originalNode.getId());
+            List<Edge> originalEdges = originalNode.getEdges();
+            for (Edge originalEdge : originalEdges) {
+                Node originalDestination = originalEdge.getDestination();
+                double originalWeight = originalEdge.getWeight();
+                Node newDestination = newGraph.getNode(originalDestination.getId());
+                newNode.addBidirectionalEdge(newDestination, originalWeight);
+            }
+        }
+
+        return newGraph;
+    }
+
     public static GraphUtils getInstance(){
         if (instance == null){
             instance = new GraphUtils();
--- app/src/test/java/com/isc/hermes/utils/GraphUtilsTest.java
+++ app/src/test/java/com/isc/hermes/utils/GraphUtilsTest.java
@@ -5,8 +5,10 @@ import com.isc.hermes.model.graph.Node;
 
 import junit.framework.TestCase;
 
-public class GraphUtilsTest extends TestCase {
+import org.junit.Test;
 
+public class GraphUtilsTest extends  TestCase{
+    @Test
     public void testTraverseGraph() {
         Graph graph = new Graph();
         Node estebanArze_Sucre = new Node("estebanArze_Sucre", -17.394251, -66.156338);
@@ -49,8 +51,79 @@ public class GraphUtilsTest extends TestCase {
         graph.addNode(jordan_sanMartin);
         graph.addNode(sucre_Lanza);
         graph.addNode(lanza_jordan);
-        System.out.println("start");
-        GraphUtils.getInstance().traverseGraph(graph);
-        System.out.println("end");
+        assertEquals("Street names in sucre_25mayo:sucre_25mayo -> sucre_sanMartinsucre_25mayo -> jordan_25mayo----*-----Street names in jordan_estebanArze:jordan_estebanArze -> estebanArze_Sucre----*-----Street names in jordan_25mayo:jordan_25mayo -> jordan_estebanArzejordan_25mayo -> calama_25mayo----*-----Street names in calama_estebanArze:calama_estebanArze -> jordan_estebanArzecalama_estebanArze -> calama_25mayo----*-----Street names in calama_25mayo:calama_25mayo -> calama_sanMartin----*-----Street names in estebanArze_Sucre:estebanArze_Sucre -> sucre_25mayo----*-----Street names in lanza_Jordan:lanza_Jordan -> jordan_sanMartin----*-----Street names in sucre_Lanza:sucre_Lanza -> lanza_Jordan----*-----Street names in calama_sanMartin:calama_sanMartin -> jordan_sanMartin----*-----Street names in sucre_sanMartin:sucre_sanMartin -> sucre_Lanza----*-----Street names in jordan_sanMartin:jordan_sanMartin -> jordan_25mayojordan_sanMartin -> sucre_sanMartin----*-----",
+                GraphUtils.getInstance().traverseGraph(graph));
+    }
+
+    @Test
+    public void testCreateBidirectionalGraph(){
+
+        Graph graph = new Graph();
+        Node a = new Node("a", -17.394251, -66.156338);
+        Node b = new Node("b", -17.394064, -66.155208);
+        Node c = new Node("c", -17.393858, -66.154149);
+        Node d = new Node("d", -17.395030, -66.155045);
+        Node e = new Node("e", -17.395276, -66.156104);
+        Node f = new Node("f", -17.396376, -66.155970);
+        Node g = new Node("g", -17.396151, -66.154875);
+        Node h = new Node("h", -17.395951, -66.153754);
+        Node i = new Node("i", -17.394903, -66.153965);
+        Node j = new Node("j", -17.393682, -66.153060);
+        Node k = new Node("k", -17.394716, -66.152910);
+
+
+        a.addUnidirectionalEdge(b);
+        b.addUnidirectionalEdge(c);
+        c.addUnidirectionalEdge(j);
+        j.addUnidirectionalEdge(k);
+        k.addUnidirectionalEdge(i);
+        b.addUnidirectionalEdge(d);
+        d.addUnidirectionalEdge(e);
+        d.addUnidirectionalEdge(g);
+        e.addUnidirectionalEdge(a);
+        i.addUnidirectionalEdge(d);
+        f.addUnidirectionalEdge(e);
+        f.addUnidirectionalEdge(g);
+        g.addUnidirectionalEdge(h);
+        h.addUnidirectionalEdge(i);
+        i.addUnidirectionalEdge(c);
+
+        graph.addNode(a);
+        graph.addNode(b);
+        graph.addNode(c);
+        graph.addNode(d);
+        graph.addNode(e);
+        graph.addNode(f);
+        graph.addNode(g);
+        graph.addNode(h);
+        graph.addNode(i);
+        graph.addNode(j);
+        graph.addNode(k);
+
+
+        Graph hello = GraphUtils.getInstance().createBidirectionalGraph(graph);
+        assert hello.getNode("a").getEdges().stream().anyMatch(edge -> edge.getDestination().getId().equals("b"));
+        assert hello.getNode("a").getEdges().stream().anyMatch(edge -> edge.getDestination().getId().equals("e"));
+        assert hello.getNode("b").getEdges().stream().anyMatch(edge -> edge.getDestination().getId().equals("a"));
+        assert hello.getNode("b").getEdges().stream().anyMatch(edge -> edge.getDestination().getId().equals("c"));
+        assert hello.getNode("b").getEdges().stream().anyMatch(edge -> edge.getDestination().getId().equals("d"));
+        assert hello.getNode("c").getEdges().stream().anyMatch(edge -> edge.getDestination().getId().equals("b"));
+        assert hello.getNode("c").getEdges().stream().anyMatch(edge -> edge.getDestination().getId().equals("i"));
+        assert hello.getNode("d").getEdges().stream().anyMatch(edge -> edge.getDestination().getId().equals("b"));
+        assert hello.getNode("d").getEdges().stream().anyMatch(edge -> edge.getDestination().getId().equals("e"));
+        assert hello.getNode("d").getEdges().stream().anyMatch(edge -> edge.getDestination().getId().equals("g"));
+        assert hello.getNode("d").getEdges().stream().anyMatch(edge -> edge.getDestination().getId().equals("i"));
+        assert hello.getNode("e").getEdges().stream().anyMatch(edge -> edge.getDestination().getId().equals("a"));
+        assert hello.getNode("e").getEdges().stream().anyMatch(edge -> edge.getDestination().getId().equals("d"));
+        assert hello.getNode("f").getEdges().stream().anyMatch(edge -> edge.getDestination().getId().equals("e"));
+        assert hello.getNode("f").getEdges().stream().anyMatch(edge -> edge.getDestination().getId().equals("g"));
+        assert hello.getNode("g").getEdges().stream().anyMatch(edge -> edge.getDestination().getId().equals("d"));
+        assert hello.getNode("g").getEdges().stream().anyMatch(edge -> edge.getDestination().getId().equals("f"));
+        assert hello.getNode("g").getEdges().stream().anyMatch(edge -> edge.getDestination().getId().equals("h"));
+        assert hello.getNode("h").getEdges().stream().anyMatch(edge -> edge.getDestination().getId().equals("g"));
+        assert hello.getNode("h").getEdges().stream().anyMatch(edge -> edge.getDestination().getId().equals("i"));
+        assert hello.getNode("i").getEdges().stream().anyMatch(edge -> edge.getDestination().getId().equals("c"));
+        assert hello.getNode("i").getEdges().stream().anyMatch(edge -> edge.getDestination().getId().equals("d"));
+        assert hello.getNode("i").getEdges().stream().anyMatch(edge -> edge.getDestination().getId().equals("h"));
     }
 }
