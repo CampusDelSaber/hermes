d5b3869a6fba99d308734040aa34fb4890c4f1c5
Daniel Espinoza <daniel.espinoza.8812451@usalesiana.edu.bo>
2023-06-20 17:35:37 -0400

    chore : rebase to good version

--- app/src/main/java/com/isc/hermes/view/OfflineCardView.java
+++ app/src/main/java/com/isc/hermes/view/OfflineCardView.java
@@ -1,5 +1,4 @@
 package com.isc.hermes.view;
-
 import android.content.Context;
 import android.graphics.Typeface;
 import android.util.TypedValue;
@@ -8,47 +7,39 @@ import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.PopupMenu;
 import android.widget.TextView;
-
 import androidx.cardview.widget.CardView;
-
 import com.isc.hermes.R;
-
 import de.hdodenhof.circleimageview.CircleImageView;
-
 /**
  * This class is responsible for creating CardViews.
  */
 public class OfflineCardView {
 
-    private final Context context;
+    private Context context;
 
     public OfflineCardView(Context context) {
         this.context = context;
     }
 
     /**
-     * This method creates a new card view for a downloaded map.
+     * Creates a new card view for a downloaded map.
      *
      * @param name The name of the downloaded region.
-     * @param size The size of the downloaded region.
      */
-    public CardView createNewCardMapDownloaded(String name, String size, PopupMenu.OnMenuItemClickListener onClickListener) {
-        LinearLayout textViewsVertical = createLinearLayout(LinearLayout.VERTICAL);
+    public CardView createNewCardMapDownloaded(String name, PopupMenu.OnMenuItemClickListener onClickListener) {
+        LinearLayout textViewsVertical = createVerticalLinearLayout();
         textViewsVertical.setPadding(100, 0, 100, 0);
-        textViewsVertical.addView(createTextView(name, 18));
-        textViewsVertical.addView(createTextView(size, 15));
-        return joinComponents(createCircleImageView(null, R.drawable.img_check,30,10),
-                textViewsVertical,
-                createCircleImageView(onClickListener,R.drawable.img_tree_points_vertical, 10,30)
-        );
+        textViewsVertical.addView(createTextView(name, 18, Typeface.BOLD));
+        return joinComponents(createCheckImageView(), textViewsVertical, createButtonPopup(onClickListener));
     }
 
     /**
-     * This method displays the options menu of a downloaded map.
+     * This method shows a menu popup
      *
-     * @param view the component that triggered the action
-     * @param onClickListener responsible for receiving the selected menu item
+     * @param view            parent view
+     * @param onClickListener listener hears when a button is clicked
      */
+
     public void showPopupMenu(View view, PopupMenu.OnMenuItemClickListener onClickListener) {
         PopupMenu popupMenu = new PopupMenu(context, view);
         popupMenu.setOnMenuItemClickListener(onClickListener);
@@ -57,51 +48,68 @@ public class OfflineCardView {
     }
 
     /**
-     * This method creates a CircleImageView.
+     * Creates a check image as a CircleImageView.
+     *
+     * @return The created check image.
+     */
+    private CircleImageView createCheckImageView() {
+        CircleImageView checkImageView = new CircleImageView(context);
+        checkImageView.setImageResource(R.drawable.img_check);
+        checkImageView.setPadding(30, 10, 10, 0);
+        checkImageView.setLayoutParams(new ViewGroup.LayoutParams(100, 100));
+        return checkImageView;
+    }
+
+
+    /**
+     * Creates a popup button as a CircleImageView.
      *
      * @return The created popup button.
      */
-    private CircleImageView createCircleImageView(PopupMenu.OnMenuItemClickListener onClickListener,int resID, int left, int right) {
-        CircleImageView circleImageView = new CircleImageView(context);
-        circleImageView.setImageResource(resID);
-        circleImageView.setPadding(left, 10, right, 0);
-        circleImageView.setLayoutParams(new ViewGroup.LayoutParams(100, 100));
-        if(onClickListener != null){
-            circleImageView.setOnClickListener(v -> showPopupMenu(v, onClickListener));
-        }
-        return circleImageView;
+    private CircleImageView createButtonPopup(PopupMenu.OnMenuItemClickListener onClickListener) {
+        CircleImageView popupButton = new CircleImageView(context);
+        popupButton.setImageResource(R.drawable.img_tree_points_vertical);
+        popupButton.setPadding(10, 10, 30, 0);
+        popupButton.setLayoutParams(new ViewGroup.LayoutParams(100, 100));
+        popupButton.setOnClickListener(v -> showPopupMenu(v, onClickListener));
+        return popupButton;
     }
 
     /**
-     * This method creates a LinearLayout.
+     * Creates a vertical LinearLayout.
      *
      * @return The created vertical LinearLayout.
      */
-    private LinearLayout createLinearLayout(int orientation) {
+    private LinearLayout createVerticalLinearLayout() {
         LinearLayout linearLayout = new LinearLayout(context);
-        linearLayout.setOrientation(orientation);
-        linearLayout.setLayoutParams(new CardView.LayoutParams(CardView.LayoutParams.MATCH_PARENT, CardView.LayoutParams.WRAP_CONTENT));
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        linearLayout.setLayoutParams(new LinearLayout.LayoutParams(
+                0,
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                0.6f
+        ));
         return linearLayout;
     }
 
     /**
      * Creates a TextView with the specified text, text size, and text style.
      *
-     * @param text     The text to be displayed.
-     * @param textSize The size of the text.
+     * @param text      The text to be displayed.
+     * @param textSize  The size of the text.
+     * @param textStyle The style of the text.
      * @return The created TextView.
      */
-    private TextView createTextView(String text, int textSize) {
+    private TextView createTextView(String text, int textSize, int textStyle) {
         TextView textView = new TextView(context);
         textView.setText(text);
         textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, textSize);
-        textView.setTypeface(null, Typeface.BOLD);
+        textView.setTypeface(null, textStyle);
         textView.setPadding(0, 10, 0, 10);
         return textView;
     }
 
     /**
-     * This method joins the components (check image, text views, and popup button) to create a CardView.
+     * Joins the components (check image, text views, and popup button) to create a CardView.
      *
      * @param checkImage    The check image.
      * @param vBoxTextViews A vertical LinearLayout containing the text views.
@@ -111,17 +119,15 @@ public class OfflineCardView {
     private CardView joinComponents(CircleImageView checkImage, LinearLayout vBoxTextViews, CircleImageView popupMenu) {
         CardView cardView = new CardView(context);
         cardView.setLayoutParams(new CardView.LayoutParams(CardView.LayoutParams.MATCH_PARENT, CardView.LayoutParams.WRAP_CONTENT));
-        LinearLayout container = createLinearLayout(LinearLayout.HORIZONTAL);
+        LinearLayout container = new LinearLayout(context);
+        container.setLayoutParams(new CardView.LayoutParams(CardView.LayoutParams.MATCH_PARENT, CardView.LayoutParams.WRAP_CONTENT));
+        container.setOrientation(LinearLayout.HORIZONTAL);
         cardView.addView(container);
 
         cardView.setPadding(10, 10, 10, 10);
-
         container.addView(checkImage);
         container.addView(vBoxTextViews);
         container.addView(popupMenu);
-
         return cardView;
     }
-
-
 }
