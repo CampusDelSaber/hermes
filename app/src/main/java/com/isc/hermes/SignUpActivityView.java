e3f622d3be41f62f89e4dbc42f60a30766ed5e5b
jeferssoncl77 <jefersson.coronel.13622689@usalesiana.edu.bo>
2023-06-22 21:18:59 -0400

    feat: refactorization and update of implementation to CRUD operations and verification

--- app/src/main/java/com/isc/hermes/SignUpActivityView.java
+++ app/src/main/java/com/isc/hermes/SignUpActivityView.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.common.api.ApiException;
 import com.isc.hermes.controller.authentication.AuthenticationFactory;
@@ -28,9 +30,10 @@ import timber.log.Timber;
  * This class is in charge of controlling the user's authentication activity.
  */
 public class SignUpActivityView extends AppCompatActivity {
-    private static final int REQUEST_CODE = 7;
     private Map<AuthenticationServices, IAuthentication> authenticationServices;
     public static IAuthentication authenticator;
+    private static final int REQUEST_CODE = 7;
+    public static String idUserLogged;
 
     /**
      * Method for creating the view and configuring it using the components xml.
@@ -108,10 +111,27 @@ public class SignUpActivityView extends AppCompatActivity {
      *
      * @param user The User object to be sent to the other activity.
      */
-    private void sendUserBetweenActivities(User user) {
-        Intent intent = new Intent(this, UserSignUpCompletionActivity.class);
-        intent.putExtra("userObtained", user);
-        startActivity(intent);
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void changeActivityDependingIsUserIsRegistered(User user) throws ExecutionException, InterruptedException, JSONException {
+        AccountInfoManager accountInfoManager = new AccountInfoManager();
+        Intent intent;
+
+        if (accountInfoManager.verifyIfAccountIsRegistered(user.getEmail())) {
+            intent = new Intent(this, MainActivity.class);
+            idUserLogged = accountInfoManager.getIdByEmail(user.getEmail());
+            System.out.println(idUserLogged);
+            System.out.println(idUserLogged);
+            System.out.println(idUserLogged);
+            System.out.println(idUserLogged);
+            System.out.println(idUserLogged);
+            System.out.println(idUserLogged);
+            System.out.println(idUserLogged);
+            System.out.println(idUserLogged);
+            System.out.println(idUserLogged);
+        } else {
+            intent = new Intent(this, UserSignUpCompletionActivity.class);
+            intent.putExtra("userObtained", user);
+        } startActivity(intent);
     }
 
     /**
@@ -123,14 +143,13 @@ public class SignUpActivityView extends AppCompatActivity {
      * @param resultCode  The integer result code returned by the child activity
      * @param data        An Intent, which can return result data to the caller
      */
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         try {
-            if (requestCode == REQUEST_CODE && resultCode == RESULT_OK){
-                    sendUserBetweenActivities(authenticator.getUserBySignInResult(data));
-            }
-        } catch (ApiException e) {
+            changeActivityDependingIsUserIsRegistered(authenticator.getUserBySignInResult(data));
+        } catch (ExecutionException | InterruptedException | JSONException | ApiException e) {
             Toast.makeText(SignUpActivityView.this,"Wait a moment ",
                     Toast.LENGTH_SHORT).show();
             Timber.tag("LOG").e(e);
