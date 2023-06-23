d4ef9619eb850ceb9160e25c9093e27fd8374b41
Fernando Mauricio Mamani Navarro <fernando.mamani.13389201@usalesiana.edu.bo>
2023-06-22 23:16:11 -0400

    task:addinTextDistanceAndTime

--- app/src/main/java/com/isc/hermes/controller/InfoRouteController.java
+++ app/src/main/java/com/isc/hermes/controller/InfoRouteController.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -14,6 +15,11 @@ import com.isc.hermes.R;
 import com.isc.hermes.model.Utils.MapPolyline;
 import com.isc.hermes.utils.Animations;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,18 +27,29 @@ public class InfoRouteController {
     private ConstraintLayout layout;
     private boolean isActive;
     private Button cancelButton;
-    private static InfoRouteController instanceNavigationController;
-    ArrayList<Integer> colorsInfoRoutes = new ArrayList<>();
+    private Button buttonRouteA;
+    private Button buttonRouteB;
+    private Button buttonRouteC;
+    private TextView timeText;
+    private TextView distanceText;
+    private static InfoRouteController instanceNavigationController;ArrayList<Integer> colorsInfoRoutes = new ArrayList<>();
     ArrayList<String> geoJsonRoutes;
     MapPolyline mapPolyline;
+    ArrayList<JSONObject> jsonObjects;
 
     private InfoRouteController(Context context){
         layout = ((AppCompatActivity)context).findViewById(R.id.distance_time_view);
         cancelButton =  ((AppCompatActivity)context).findViewById(R.id.cancel_navigation_button);
+        timeText = ((AppCompatActivity)context).findViewById(R.id.timeText);
+        distanceText = ((AppCompatActivity)context).findViewById(R.id.distanceText);
+        buttonRouteA = ((AppCompatActivity)context).findViewById(R.id.ButtonRouteOne);
+        buttonRouteB = ((AppCompatActivity)context).findViewById(R.id.ButtonRouteTwo);
+        buttonRouteC = ((AppCompatActivity)context).findViewById(R.id.ButtonRouteThree);
         isActive = false;
         colorsInfoRoutes.add(0xFF2867DC);
         colorsInfoRoutes.add(0XFFC5D9FD);
         colorsInfoRoutes.add(0XFFC5D9FD);
+        jsonObjects = new ArrayList<>();
         setActionButtons();
     }
 
@@ -45,6 +62,9 @@ public class InfoRouteController {
             isActive = false;
         });
 
+        buttonRouteA.setOnClickListener(v -> setTimeAndDistanceInformation(jsonObjects.get(0)));
+        buttonRouteB.setOnClickListener(v -> setTimeAndDistanceInformation(jsonObjects.get(1)));
+        buttonRouteC.setOnClickListener(v -> setTimeAndDistanceInformation(jsonObjects.get(2)));
     }
 
     public void showDistanceAndTimeView(){
@@ -64,6 +84,16 @@ public class InfoRouteController {
         this.mapPolyline = mapPolyline;
         isActive = true;
         layout.setVisibility(View.VISIBLE);
+        jsonObjects = new ArrayList<>();
+        try {
+            for(String currentJson : jsonCoordinates){
+                jsonObjects.add(new JSONObject(currentJson));
+            }
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+
+
         mapPolyline.displaySavedCoordinates(jsonCoordinates, colorsInfoRoutes);
     }
     public ConstraintLayout getLayout() {
@@ -72,5 +102,33 @@ public class InfoRouteController {
 
     public boolean isActive(){
         return isActive;
+    }
+
+    private void setTimeAndDistanceInformation(JSONObject jsonObject){
+        try {
+            int timeInMinutes = jsonObject.getInt("time");
+            int hours = 0;
+            while(timeInMinutes - 60 > 0){
+                timeInMinutes -= 60;
+                hours++;
+            }
+            if(hours > 0) timeText.setText(hours+" h "+ timeInMinutes+" min" );
+            else timeText.setText(timeInMinutes+" min" );
+
+            double meters = jsonObject.getDouble("distance");
+            double kilometers = 0;
+            while( meters - 1 >= 1){
+                meters -= 1;
+                kilometers ++;
+            }
+
+            int decimals = 3;
+            DecimalFormat decimalFormat = new DecimalFormat("#." + "0".repeat(decimals));
+            if(kilometers > 0) distanceText.setText(kilometers+ " km. " + decimalFormat.format(meters).substring(1)+ " m.");
+            else distanceText.setText(decimalFormat.format(meters).substring(1)+ " m.");
+
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- app/src/main/java/com/isc/hermes/controller/NavigationOptionsController.java
+++ app/src/main/java/com/isc/hermes/controller/NavigationOptionsController.java
@@ -233,9 +233,8 @@ public class NavigationOptionsController {
         Map<String, String> r = new HashMap<>();
 
         r.put("Route A", "{\"type\":\"Feature\",\"distance\":0.5835077072636502,\"time\":10,\"properties\":{},\"geometry\":{\"type\":\"LineString\",\"coordinates\":[[-66.156338,-17.394251],[-66.155208,-17.394064],[-66.154149,-17.393858],[-66.15306,-17.393682],[-66.15291,-17.394716],[-66.153965,-17.394903]]}}");
-        r.put("Route B", "{\"type\":\"Feature\",\"distance\":0.5961126697414532,\"properties\":{},\"geometry\":{\"type\":\"LineString\",\"coordinates\":[[-66.156338,-17.394251],[-66.155208,-17.394064],[-66.155045,-17.39503],[-66.154875,-17.396151],[-66.153754,-17.395951],[-66.153965,-17.394903]]}}");
-        r.put("Route C", "{\"type\":\"Feature\",\"distance\":0.5961126697414532,\"properties\":{},\"geometry\":{\"type\":\"LineString\",\"coordinates\":[[-66.159019, -17.398311],[-66.154399, -17.397043],[-66.151315, -17.398656],[-66.147585, -17.400585],[-66.142978, -17.401595]]}}");
-
+        r.put("Route B", "{\"type\":\"Feature\",\"distance\":0.5961126697414532,\"time\":12,\"properties\":{},\"geometry\":{\"type\":\"LineString\",\"coordinates\":[[-66.156338,-17.394251],[-66.155208,-17.394064],[-66.155045,-17.39503],[-66.154875,-17.396151],[-66.153754,-17.395951],[-66.153965,-17.394903]]}}");
+        r.put("Route C", "{\"type\":\"Feature\",\"distance\":1.6061126697414532,\"time\":15,\"properties\":{},\"geometry\":{\"type\":\"LineString\",\"coordinates\":[[-66.159019, -17.398311],[-66.154399, -17.397043],[-66.151315, -17.398656],[-66.147585, -17.400585],[-66.142978, -17.401595]]}}");
         String jsonA = r.get("Route A");
         String jsonB = r.get("Route B");
         String jsonC = r.get("Route C");
@@ -244,11 +243,6 @@ public class NavigationOptionsController {
         geoJson.add(jsonA);
         geoJson.add(jsonB);
         geoJson.add(jsonC);
-
-        ArrayList<Integer> colors = new ArrayList<>();
-        colors.add(0xFF2867DC);
-        colors.add(0XFFC5D9FD);
-        colors.add(0XFFC5D9FD);
 
         MapPolyline mapPolyline = new MapPolyline(MapDisplay.getInstance(context,null,null).getMapView());
         infoRouteController.showInfoRoute(geoJson, mapPolyline);
