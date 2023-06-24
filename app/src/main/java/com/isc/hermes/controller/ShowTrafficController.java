e8e9205fd369daf8d8e82a6df580390b939dfaff
Gaston Gutierrez <gaston.gutierrez.9964359@usalesiana.edu.bo>
2023-06-24 17:25:05 -0400

    show traffic comments

--- app/src/main/java/com/isc/hermes/controller/ShowTrafficController.java
+++ app/src/main/java/com/isc/hermes/controller/ShowTrafficController.java
@@ -16,12 +16,20 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
 
+/**
+ * This class will be in charge of displaying and occluding the Traffic that you locate in the database.
+ *
+ */
 public class ShowTrafficController {
     private IncidentsDataProcessor incidentsDataProcessor = IncidentsDataProcessor.getInstance();
     private List<Polyline> trafficPolylines = new ArrayList<>();
 
+
+    /**
+     * This method will hide the incidents if the user does not want to see them.
+     * @param mapDisplay It is the map which will be shown .
+     */
     public void hideTraffic(MapDisplay mapDisplay) {
         for (Polyline polyline : trafficPolylines) {
             mapDisplay.getMapboxMap().removePolyline(polyline);
@@ -29,7 +37,10 @@ public class ShowTrafficController {
         trafficPolylines.clear();
     }
 
-
+    /**
+     * This method will show the incidents if the user want to see them.
+     * @param mapDisplay It is the map which will be shown .
+     */
     public void showTraffic(MapDisplay mapDisplay) throws JSONException {
         JSONArray incidentsArray = incidentsDataProcessor.getAllIncidentsByType("Traffic");
         assertNotNull(incidentsArray);
@@ -39,6 +50,13 @@ public class ShowTrafficController {
             trafficPolylines.add(polyline);
         }
     }
+
+    /**
+     * This method will be in charge of once filtered the data of the points stored in the database,
+     * it obtains them and draws the lines.
+     * @param mapDisplay It is the map which will be shown .
+     * @param incidentObject is the JSON file to extract the data .
+     */
     private Polyline createTrafficPolyline(MapDisplay mapDisplay, JSONObject incidentObject) throws JSONException {
         JSONObject geometryObject = incidentObject.getJSONObject("geometry");
         JSONArray coordinatesArray = geometryObject.getJSONArray("coordinates");
