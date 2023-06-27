a62af5385f50f573a5865630edab72b9b5715d20
Denis Jorge Gandarillas Delgado <denis.gandarillas.9355205@usalesiana.edu.bo>
2023-06-27 19:43:35 -0400

    Add JavaDocs

--- app/src/main/java/com/isc/hermes/model/SaveProfileImage.java
+++ app/src/main/java/com/isc/hermes/model/SaveProfileImage.java
@@ -12,16 +12,28 @@ import com.google.firebase.storage.UploadTask;
 import com.isc.hermes.database.AccountInfoManager;
 import com.isc.hermes.model.User.UserRepository;
 
+/**
+ * Utility class for saving a profile image to Firebase Storage and updating the user's path image.
+ */
 public class SaveProfileImage {
 
     private static final String FOLDER_NAME = "Profile_images";
     private static final String MESSAGE = "Image uploaded successfully!";
     private StorageReference storageReference;
 
+    /**
+     * Constructs a new SaveProfileImage instance.
+     */
     public SaveProfileImage() {
         storageReference = FirebaseStorage.getInstance().getReference();
     }
 
+    /**
+     * Saves the profile image to Firebase Storage and updates the user's path image.
+     *
+     * @param uri     The Uri of the image to be saved.
+     * @param context The context in which the method is called.
+     */
     public void saveProfileImage(Uri uri, Context context) {
         StorageReference filePath = storageReference.child(FOLDER_NAME).child(uri.getLastPathSegment());
         filePath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
@@ -39,6 +51,11 @@ public class SaveProfileImage {
         });
     }
 
+    /**
+     * Sets the path of the profile image for the current user and updates the user information.
+     *
+     * @param path The path of the profile image.
+     */
     private void setPathImage(String path) {
         UserRepository.getInstance().getUserContained().setPathImageUser(path);
         AccountInfoManager accountInfoManager = new AccountInfoManager();
