a76c3f47513897f07b63d14ce6ee3fb43b557217
VictorCespedesCC <victor.cespedes.9374448@usalesiana.edu.bo>
2023-06-28 00:06:17 -0400

    solve problems

--- app/src/main/java/com/isc/hermes/OfflineMapsActivity.java
+++ app/src/main/java/com/isc/hermes/OfflineMapsActivity.java
@@ -4,6 +4,7 @@ import static com.isc.hermes.ActivitySelectRegion.MAP_CENTER_LATITUDE;
 import static com.isc.hermes.ActivitySelectRegion.MAP_CENTER_LONGITUDE;
 import static com.isc.hermes.ActivitySelectRegion.ZOOM_LEVEL;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -23,6 +24,7 @@ import com.isc.hermes.model.RegionData;
 import com.isc.hermes.controller.offline.CardViewHandler;
 import com.isc.hermes.view.OfflineCardView;
 import com.isc.hermes.utils.offline.MapboxOfflineManager;
+import com.isc.hermes.utils.offline.OfflineUtils;
 import com.isc.hermes.controller.offline.RegionDeleter;
 import com.isc.hermes.controller.offline.RegionDownloader;
 import com.isc.hermes.controller.offline.RegionLoader;
@@ -32,9 +34,8 @@ import com.mapbox.mapboxsdk.offline.OfflineRegion;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-import java.nio.charset.StandardCharsets;
-import timber.log.Timber;
 
+import java.io.UnsupportedEncodingException;
 
 /**
  * This class represents the Offline Mode Settings UI.
@@ -243,26 +244,27 @@ public class OfflineMapsActivity extends AppCompatActivity implements RegionObse
      */
     private void uploadNameRegion(String previousName, String newName){
         OfflineRegion targetRegion = MapboxOfflineManager.getInstance(this).getOfflineRegion(previousName);
-        byte[] metadata = targetRegion.getMetadata();
 
         try {
-            JSONObject jsonMetadata = new JSONObject(new String(metadata, StandardCharsets.UTF_8));
-            jsonMetadata.put("name", newName);
-            byte[] newMetadata = jsonMetadata.toString().getBytes(StandardCharsets.UTF_8);
-            targetRegion.updateMetadata(newMetadata,new OfflineRegion.OfflineRegionUpdateMetadataCallback() {
+            JSONObject jsonMetadata = new JSONObject(new String(OfflineUtils.createMetadata(previousName), OfflineUtils.JSON_CHARSET));
+            jsonMetadata.put(OfflineUtils.JSON_FIELD_REGION_NAME, newName);
+
+            Context context = this;
+            targetRegion.updateMetadata(jsonMetadata.toString().getBytes(OfflineUtils.JSON_CHARSET),new OfflineRegion.OfflineRegionUpdateMetadataCallback() {
                 @Override
                 public void onUpdate(byte[] updatedMetadata) {
-                    Timber.i("Map name has been updated");
+                    Toast.makeText(context, "Map name has been updated", Toast.LENGTH_SHORT).show();
                 }
-
                 @Override
                 public void onError(String error) {
-                    Timber.i("Error:%s", error);
+                    Toast.makeText(context, "Error:%s" + error, Toast.LENGTH_SHORT).show();
                 }
             });
-        } catch (JSONException e) {
+            MapboxOfflineManager.getInstance(this).getOfflineRegions().put(newName, targetRegion);
+            MapboxOfflineManager.getInstance(this).getOfflineRegions().remove(previousName);
+        } catch (JSONException | UnsupportedEncodingException e) {
             e.printStackTrace();
-            Timber.i("An error occurred while updating the map");
+            Toast.makeText(this, "An error occurred while updating the map", Toast.LENGTH_SHORT).show();
         }
     }
 }
