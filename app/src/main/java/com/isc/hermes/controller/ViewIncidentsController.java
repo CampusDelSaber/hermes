eff69d7b51c5bfc528299246f460ef08e9e66680
Gaston Gutierrez <gaston.gutierrez.9964359@usalesiana.edu.bo>
2023-06-27 23:26:07 -0400

    conflict merge

--- app/src/main/java/com/isc/hermes/controller/ViewIncidentsController.java
+++ app/src/main/java/com/isc/hermes/controller/ViewIncidentsController.java
@@ -32,14 +32,10 @@ public class ViewIncidentsController{
     private Button okButton;
     private final Button cancelButton;
     private final ConstraintLayout displayIncidents;
-    private final NaturalDisasterRequesting requesting;
-    private boolean isTrafficVisible = false;
-    private ShowTrafficController showTrafficController = new ShowTrafficController();
 
 
     public ViewIncidentsController(AppCompatActivity activity){
         this.activity = activity;
-        this.requesting = new NaturalDisasterRequesting();
         displayIncidentsButton = activity.findViewById(R.id.displayIncidentsButton);
         displayIncidents = activity.findViewById(R.id.display_incidents);
         okButton = activity.findViewById(R.id.okButton);
@@ -48,40 +44,9 @@ public class ViewIncidentsController{
     }
 
     /**
-     *This method displays the incidents screen.
-     *
-     * The types of incidents that you have in the application will
-     * be displayed where you can mark or unmark the incidents that
-     * you want to be shown.
-     */
-    /**
      * Method to init the action about click on a single button on show incidents form
      */
-    private void initViewDifferentIncidentsTypeButton(ActivitySelectRegion mapDisplay) {
-        viewTrafficButton.setOnClickListener(v -> {
-            try {
-                if (isTrafficVisible) {
-                    showTrafficController.hideTraffic(mapDisplay);
-                    isTrafficVisible = false;
-                } else {
-                    showTrafficController.showTraffic(mapDisplay);
-                    isTrafficVisible = true;
-                }
-            } catch (JSONException e) {
-                throw new RuntimeException(e);
-            }
-            disappearForm();
-        });
 
-        viewSocialEventButton.setOnClickListener(v -> {
-            disappearForm();
-        });
-
-        viewNaturalDisasterButton.setOnClickListener(v -> {
-            PolygonVisualizationController.getInstance().displayPointsPolygonOnMap(requesting.getAllPolygonPoints(),"#Ff0000");
-            disappearForm();
-        });
-    }
     private void showIncidentsScreen() {
         IncidentViewNavigation.getInstance(activity)
                 .initIncidentButtonFunctionality(displayIncidentsButton, displayIncidents);
