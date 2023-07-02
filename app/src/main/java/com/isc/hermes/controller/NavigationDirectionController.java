d6d9e1739b11a6324ad0b75dbc20b052353120fa
aguirrekarina <karina.aguirre.9491656@usalesiana.edu.bo>
2023-07-02 18:09:27 -0400

    set of components view

--- app/src/main/java/com/isc/hermes/controller/NavigationDirectionController.java
+++ app/src/main/java/com/isc/hermes/controller/NavigationDirectionController.java
@@ -1,14 +1,14 @@
 package com.isc.hermes.controller;
 
 import android.content.Context;
-import android.location.Address;
-import android.location.Geocoder;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import com.isc.hermes.R;
+import com.isc.hermes.model.navigation.directions.DirectionEnum;
 import com.isc.hermes.model.navigation.directions.DirectionsRecord;
 import com.isc.hermes.utils.Animations;
 
@@ -18,8 +18,10 @@ import com.isc.hermes.utils.Animations;
 public class NavigationDirectionController {
     private final Context context;
     private final RelativeLayout directionsForm;
-    private TextView directionsList;
+    private TextView streetName;
+    private TextView direction;
     private Button closeButton;
+    private ImageView indicationImage;
 
     /**
      * Constructor for the NavigationDirectionController class.
@@ -28,9 +30,11 @@ public class NavigationDirectionController {
      */
     public NavigationDirectionController(Context context) {
         this.context = context;
-        directionsForm = ((AppCompatActivity) context).findViewById(R.id.directions_form);
-        directionsList = ((AppCompatActivity) context).findViewById(R.id.directions_list);
-        closeButton = ((AppCompatActivity) context).findViewById(R.id.close_button);
+        directionsForm = ((AppCompatActivity) context).findViewById(R.id.directions_form_real_time);
+        streetName = ((AppCompatActivity) context).findViewById(R.id.street_name);
+        direction = ((AppCompatActivity) context).findViewById(R.id.indication_text);
+        closeButton = ((AppCompatActivity) context).findViewById(R.id.close_button_2);
+        indicationImage = ((AppCompatActivity) context).findViewById(R.id.indication_image);
         setButtonsEvent();
     }
 
@@ -58,6 +62,22 @@ public class NavigationDirectionController {
     }
 
     public void update(DirectionsRecord current, DirectionsRecord next){
-        //
+        direction.setText(next.getDirection().getText());
+        streetName.setText(next.getStreetName());
+        setDirectionIcon(next.getDirection());
+    }
+
+    public void setDirectionIcon(DirectionEnum directionEnum) {
+        switch (directionEnum) {
+            case GO_STRAIGHT:
+                indicationImage.setImageResource(R.drawable.go_straight_icon);
+                break;
+            case TURN_LEFT:
+                indicationImage.setImageResource(R.drawable.turn_left_icon);
+                break;
+            case TURN_RIGHT:
+                indicationImage.setImageResource(R.drawable.turn_right_icon);
+                break;
+        }
     }
 }
