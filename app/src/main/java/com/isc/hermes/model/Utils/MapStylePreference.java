0f2ade00ddc0d2aca4ed0d11f1a004957d1ee78a
Juan Pablo Arequipa <juan.arequipa.12833847@usalesiana.edu.bo>
2023-07-02 16:21:00 -0400

    map styles stored at shared preferences

---
+++ app/src/main/java/com/isc/hermes/model/Utils/MapStylePreference.java
@@ -0,0 +1,16 @@
+package com.isc.hermes.model.Utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class MapStylePreference {
+    private static final String PREFERENCE_NAME = "MapStylePreference";
+    private static final String KEY_MAP_STYLE = "MapStyle";
+
+    public static void setMapStyle(Context context, String mapStyle) {
+        SharedPreferences preferences = context.getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString(KEY_MAP_STYLE, mapStyle);
+        editor.apply();
+    }
+}
