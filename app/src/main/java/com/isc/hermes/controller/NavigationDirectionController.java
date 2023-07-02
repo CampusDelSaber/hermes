deb9ba44a939ffd152ee546cad6910418bbc42f6
aguirrekarina <karina.aguirre.9491656@usalesiana.edu.bo>
2023-07-02 00:22:29 -0400

    the display window can be closed and the displays are scrollable

--- app/src/main/java/com/isc/hermes/controller/NavigationDirectionController.java
+++ app/src/main/java/com/isc/hermes/controller/NavigationDirectionController.java
@@ -3,10 +3,14 @@ package com.isc.hermes.controller;
 import android.content.Context;
 import android.location.Address;
 import android.location.Geocoder;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import com.isc.hermes.R;
+import com.isc.hermes.utils.Animations;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -22,6 +26,8 @@ public class NavigationDirectionController {
     private final Context context;
     private final RelativeLayout directionsForm;
     private TextView directionsList;
+    private Button closeButton;
+
 
     /**
      * Constructor for the NavigationDirectionController class.
@@ -32,6 +38,22 @@ public class NavigationDirectionController {
         this.context = context;
         directionsForm = ((AppCompatActivity) context).findViewById(R.id.directions_form);
         directionsList = ((AppCompatActivity) context).findViewById(R.id.directions_list);
+        closeButton = ((AppCompatActivity) context).findViewById(R.id.close_button);
+        setButtonsEvent();
+    }
+
+    /**
+     * Sets the start navigation and recalculate buttons on click listeners actions
+     */
+    private void setButtonsEvent(){
+
+        closeButton.setOnClickListener(event -> {
+            directionsForm.startAnimation(Animations.exitAnimation);
+            closeDirectionsForm();
+        });
+    }
+    private void closeDirectionsForm() {
+        directionsForm.setVisibility(View.GONE);
     }
 
     /**
