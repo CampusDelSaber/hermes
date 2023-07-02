60e4273e1cb9bc9a92007f9a0f112e622e411e41
Juan Pablo Arequipa <juan.arequipa.12833847@usalesiana.edu.bo>
2023-07-02 16:22:52 -0400

    map styles controller implemented

---
+++ app/src/main/java/com/isc/hermes/controller/MapStylesController.java
@@ -0,0 +1,41 @@
+package com.isc.hermes.controller;
+
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.isc.hermes.R;
+import com.isc.hermes.model.Utils.MapStylePreference;
+import com.isc.hermes.utils.MapManager;
+
+public class MapStylesController {
+    private String mapStyle;
+    private AppCompatActivity activity;
+
+    public MapStylesController(AppCompatActivity activity) {
+        this.activity = activity;
+        this.mapStyle = MapStylePreference.getMapStyle(activity.getBaseContext());
+    }
+
+    /**
+     * Opens the styles menu by toggling its visibility.
+     */
+    public void openStylesMenu() {
+        LinearLayout styleOptionsWindow = activity.findViewById(R.id.styleOptionsWindow);
+        styleOptionsWindow.setVisibility(View.VISIBLE);
+    }
+
+    /**
+     * Method to set the new map style on the current map.
+     *
+     * @param view The button's view of the style that has been pressed.
+     */
+    public void changeMapStyle(View view) {
+        LinearLayout styleOptionsWindow = activity.findViewById(R.id.styleOptionsWindow);
+        styleOptionsWindow.setVisibility(View.GONE);
+        mapStyle = ((ImageButton) view).getTag().toString();
+        MapManager.getInstance().getMapboxMap().setStyle(mapStyle);
+    }
+}
